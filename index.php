4a2dfa315ae608550f6621d9f7499fb08ccdf258
Lun <waylunly@gmail.com>
2015-11-07 11:31:48 -0800

    add picture

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <html>
 <head>
 	<title>Flight with friend</title>
+
+<style>
+html, body { height: 100% }
+
+body {
+  margin: 0 auto;
+  background: grey;
+  text-align: center;
+  background-image: url("http://25.media.tumblr.com/211b5f4f19038a3940a16c7bcd7c84de/tumblr_mq7at6uz641st5lhmo1_1280.jpg");
+  background-size: cover;
+  background-position: center center;
+}
+
+input[type="file"] {
+  display: none
+}
+
+
+
+</style>
+
+
 </head>
 <body>
 
